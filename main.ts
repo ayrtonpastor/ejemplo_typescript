dc0b169cf69081a7e76e2d76345e54eb8eb5dfe0
Ayrton <a.pastor@uniandes.edu.co>
2021-04-17 02:45:53 +0000

    Se realizó primera tarea de proyecto ejemplo

---
+++ main.ts
@@ -0,0 +1,5 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+
+export const ap = new Aprendiz("Ayrton Aldair","Pastor Castillo","avatar.png",26, NivelEducativo.BACHILLERATO);
+
+console.log(ap);
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+export var ap = new Aprendiz("Ayrton Aldair", "Pastor Castillo", "avatar.png", 26, NivelEducativo.BACHILLERATO);
+console.log(ap);
