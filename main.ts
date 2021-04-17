2568a77a1c0e29edc1c57b03b46f0cd134cba91f
Ayrton <a.pastor@uniandes.edu.co>
2021-04-17 03:51:13 +0000

    Se terminó tercera tarea de proyecto de prueba

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+import { Curso } from "./curso.js";
 
-export const ap = new Aprendiz("Ayrton Aldair","Pastor Castillo","avatar.png",26, NivelEducativo.BACHILLERATO);
+let cursos = [new Curso("Prácticas escenciales para el agilismo",20,90,true, 2019),
+new Curso("Ingeniería de software para la web",15,99,true, 2018),
+new Curso("Pruebas automatizadas",25,50,false, 2020),
+new Curso("PPrincipios de diseño y arquitectura",30,75,true, 2020)];
+
+export const ap = new Aprendiz("Ayrton Aldair","Pastor Castillo","avatar.png",26, NivelEducativo.BACHILLERATO, cursos);
+console.log(ap.cursos);
 
 let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+let estadisticasTable: HTMLElement = document.getElementById("estadisticas")!;
 
 mostrarDatosAprendiz(ap);
+mostrarEstadisticas(ap);
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
     let tbodyAprendiz = document.createElement("tbody");
@@ -14,4 +23,11 @@ function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
     <tr><td>Nivel Educativo:</td><td>${aprendiz.nivelEducativo}</td></tr>
     <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`;
     aprendizTable.appendChild(tbodyAprendiz);
+}
+
+function mostrarEstadisticas(aprendiz: Aprendiz):void{
+    let numeroCertificados: number = aprendiz.darCursosCertificados();
+    let trElement: HTMLElement = document.createElement("tr");
+    trElement.innerHTML = `<td><b>Cursos certificados</b></td><td>${numeroCertificados}</td>`;
+    estadisticasTable.appendChild(trElement);
 }
