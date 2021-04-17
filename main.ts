6fa33f559c7637a5a2df527c66b13e3ef5577e3d
Ayrton <a.pastor@uniandes.edu.co>
2021-04-17 04:05:39 +0000

    Se terminó cuarta tarea de proyecto de prueba

--- main.ts
+++ main.ts
@@ -4,16 +4,18 @@ import { Curso } from "./curso.js";
 let cursos = [new Curso("Prácticas escenciales para el agilismo",20,90,true, 2019),
 new Curso("Ingeniería de software para la web",15,99,true, 2018),
 new Curso("Pruebas automatizadas",25,50,false, 2020),
-new Curso("PPrincipios de diseño y arquitectura",30,75,true, 2020)];
+new Curso("Principios de diseño y arquitectura",30,75,true, 2020)];
 
 export const ap = new Aprendiz("Ayrton Aldair","Pastor Castillo","avatar.png",26, NivelEducativo.BACHILLERATO, cursos);
 console.log(ap.cursos);
 
 let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
 let estadisticasTable: HTMLElement = document.getElementById("estadisticas")!;
+let cursosTable: HTMLElement = document.getElementById("cursos")!;
 
 mostrarDatosAprendiz(ap);
 mostrarEstadisticas(ap);
+mostrarCursosAprendiz(ap);
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
     let tbodyAprendiz = document.createElement("tbody");
@@ -30,4 +32,18 @@ function mostrarEstadisticas(aprendiz: Aprendiz):void{
     let trElement: HTMLElement = document.createElement("tr");
     trElement.innerHTML = `<td><b>Cursos certificados</b></td><td>${numeroCertificados}</td>`;
     estadisticasTable.appendChild(trElement);
+}
+
+function mostrarCursosAprendiz(aprendiz: Aprendiz):void{
+    let cursosTbody: HTMLElement = document.createElement("tbody");
+    for(let curso of aprendiz.cursos){
+        let trElement: HTMLElement = document.createElement("tr");
+        trElement.innerHTML = `<td>${curso.nombre}</td>
+        <td>${curso.horas}</td>
+        <td>${curso.calificacion}</td>
+        <td>${curso.certificado}</td>
+        <td>${curso.anio}</td>`;
+        cursosTbody.appendChild(trElement);
+    }
+    cursosTable.appendChild(cursosTbody);
 }
--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,15 @@ import { Curso } from "./curso.js";
 var cursos = [new Curso("Prácticas escenciales para el agilismo", 20, 90, true, 2019),
     new Curso("Ingeniería de software para la web", 15, 99, true, 2018),
     new Curso("Pruebas automatizadas", 25, 50, false, 2020),
-    new Curso("PPrincipios de diseño y arquitectura", 30, 75, true, 2020)];
+    new Curso("Principios de diseño y arquitectura", 30, 75, true, 2020)];
 export var ap = new Aprendiz("Ayrton Aldair", "Pastor Castillo", "avatar.png", 26, NivelEducativo.BACHILLERATO, cursos);
 console.log(ap.cursos);
 var aprendizTable = document.getElementById("aprendiz");
 var estadisticasTable = document.getElementById("estadisticas");
+var cursosTable = document.getElementById("cursos");
 mostrarDatosAprendiz(ap);
 mostrarEstadisticas(ap);
+mostrarCursosAprendiz(ap);
 function mostrarDatosAprendiz(aprendiz) {
     var tbodyAprendiz = document.createElement("tbody");
     tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./" + aprendiz.avatar + "\" height=\"100\"></td></tr>\n    <tr><td>Nombres:</td><td>" + aprendiz.nombres + "</td></tr>\n    <tr><td>Apellidos:</td><td>" + aprendiz.apellidos + "</td></tr>\n    <tr><td>Nivel Educativo:</td><td>" + aprendiz.nivelEducativo + "</td></tr>\n    <tr><td>Edad:</td><td>" + aprendiz.edad + "</td></tr>";
@@ -20,4 +22,14 @@ function mostrarEstadisticas(aprendiz) {
     var trElement = document.createElement("tr");
     trElement.innerHTML = "<td><b>Cursos certificados</b></td><td>" + numeroCertificados + "</td>";
     estadisticasTable.appendChild(trElement);
+}
+function mostrarCursosAprendiz(aprendiz) {
+    var cursosTbody = document.createElement("tbody");
+    for (var _i = 0, _a = aprendiz.cursos; _i < _a.length; _i++) {
+        var curso = _a[_i];
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td>" + curso.nombre + "</td>\n        <td>" + curso.horas + "</td>\n        <td>" + curso.calificacion + "</td>\n        <td>" + curso.certificado + "</td>\n        <td>" + curso.anio + "</td>";
+        cursosTbody.appendChild(trElement);
+    }
+    cursosTable.appendChild(cursosTbody);
 }
